cd1eaf27d804c5f2819cfe7a7e498698cc31c7f3
Marcin <nicaramus@gmail.com>
2017-04-23 14:07:35 +0000

    Link, images added; navigation to login made, deleted unneccesary imports

--- rails/ostinato/app/controllers/pages_controller.rb
+++ rails/ostinato/app/controllers/pages_controller.rb
@@ -2,7 +2,4 @@ class PagesController < ApplicationController
   def show
     render template: "pages/#{params[:page]}"
   end
-  def login
-    render template: "pages/#{params[:page]}"
-  end
 end
--- rails/ostinato/config/routes.rb
+++ rails/ostinato/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   get "/pages/:page" => "pages#show"
   root "pages#show", page: "home"
-  get "/:pages/:page" => "pages#login"
-  root "pages#login", page: "login"
 end
