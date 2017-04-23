8709c5e90ba18b30d1d365fa3a6fb6d8d9b0e0fd
_martino <marcin.krykowski@gmail.com>
2017-04-23 12:53:10 +0200

    Poprawki widokow

--- rails/ostinato/app/assets/javascripts/application.js
+++ rails/ostinato/app/assets/javascripts/application.js
@@ -15,3 +15,15 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+var loginLink = document.querySelector("span.login");
+var registerLink = document.querySelector("span.register");
+
+loginLink.addEventListener("click", function() {
+    sessionStorage.isSignIn = true;
+    window.location.href = "login.html";
+});
+
+registerLink.addEventListener("click", function() {
+    sessionStorage.isSignIn = false;
+    window.location.href = "login.html";
+});
--- rails/ostinato/config/routes.rb
+++ rails/ostinato/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "/pages/:page" => "pages#show"
   root "pages#show", page: "home"
-  get "/:pages/:page" => "pages#show"
-  root "pages#show", page: "login"
+  get "/:pages/:page" => "pages#login"
+  root "pages#login", page: "login"
 end
