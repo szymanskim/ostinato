b36f9fe0dd349541bc7ae2b2b568a9b0d495a5fb
Merlin <nicaramus@gmail.com>
2017-05-15 22:21:21 +0200

    Added static pages, facebook like box

--- rails/ostinato/db/schema.rb
+++ rails/ostinato/db/schema.rb
@@ -11,79 +11,79 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170507112722) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'comments', force: :cascade do |t|
-    t.integer  'index',      null: false
-    t.text     'content',    null: false
-    t.integer  'post_id',    null: false
-    t.integer  'user_id',    null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['post_id'], name: 'index_comments_on_post_id', using: :btree
-    t.index ['user_id'], name: 'index_comments_on_user_id', using: :btree
+  create_table "comments", force: :cascade do |t|
+    t.integer  "index",      null: false
+    t.text     "content",    null: false
+    t.integer  "post_id",    null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
-  create_table 'file_resources', force: :cascade do |t|
-    t.string   'path', null: false
-    t.text     'description'
-    t.datetime 'created_at',  null: false
-    t.datetime 'updated_at',  null: false
-    t.integer  'user_id'
-    t.index ['user_id'], name: 'index_file_resources_on_user_id', using: :btree
+  create_table "file_resources", force: :cascade do |t|
+    t.string   "path",        null: false
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_file_resources_on_user_id", using: :btree
   end
 
-  create_table 'likes', force: :cascade do |t|
-    t.integer  'user_id',     null: false
-    t.integer  'likeable_id', null: false
-    t.datetime 'created_at',  null: false
-    t.datetime 'updated_at',  null: false
-    t.index ['likeable_id'], name: 'index_likes_on_likeable_id', using: :btree
-    t.index ['user_id'], name: 'index_likes_on_user_id', using: :btree
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",     null: false
+    t.integer  "likeable_id", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["likeable_id"], name: "index_likes_on_likeable_id", using: :btree
+    t.index ["user_id"], name: "index_likes_on_user_id", using: :btree
   end
 
-  create_table 'posts', force: :cascade do |t|
-    t.text     'content', null: false
-    t.string   'title'
-    t.integer  'user_id',    null: false
-    t.integer  'topic_id',   null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['topic_id'], name: 'index_posts_on_topic_id', using: :btree
-    t.index ['user_id'], name: 'index_posts_on_user_id', using: :btree
+  create_table "posts", force: :cascade do |t|
+    t.text     "content",    null: false
+    t.string   "title"
+    t.integer  "user_id",    null: false
+    t.integer  "topic_id",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_posts_on_topic_id", using: :btree
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
-  create_table 'topics', force: :cascade do |t|
-    t.integer  'type'
-    t.string   'codename'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "topics", force: :cascade do |t|
+    t.integer  "type"
+    t.string   "codename"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string   'name',                                null: false
-    t.string   'surname',                             null: false
-    t.integer  'group', default: 0, null: false
-    t.string   'tel'
-    t.text     'description'
-    t.datetime 'created_at',                          null: false
-    t.datetime 'updated_at',                          null: false
-    t.string   'email',                  default: '', null: false
-    t.string   'encrypted_password',     default: '', null: false
-    t.string   'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer  'sign_in_count', default: 0, null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.inet     'current_sign_in_ip'
-    t.inet     'last_sign_in_ip'
-    t.index ['email'], name: 'index_users_on_email', unique: true, using: :btree
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token',
-                                      unique: true, using: :btree
+  create_table "users", force: :cascade do |t|
+    t.string   "name",                                null: false
+    t.string   "surname",                             null: false
+    t.integer  "group",                  default: 0,  null: false
+    t.string   "tel"
+    t.text     "description"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key 'file_resources', 'users'
-  add_foreign_key 'likes', 'users'
+  add_foreign_key "file_resources", "users"
+  add_foreign_key "likes", "users"
 end
